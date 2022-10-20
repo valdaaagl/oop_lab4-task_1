8c2af0e8176c0d1aca9670fadba770a94ba78e3a
vlada <vsh_o@ukr.net>
2022-10-20 13:54:01 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Printer
+    {
+        public virtual void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine(value);
+        }
+ 
+    }
+ 
+    internal class PrinterBlue : Printer
+    {
+        public override void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine(value);
+        }
+    }
+ 
+    internal class PrinterRed : Printer
+    {
+        public new void Print(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(value);
+        }
+ 
+    }
+ 
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arr = new Printer[3];
+            arr[0] = new Printer();
+            arr[1] = new PrinterBlue();
+            arr[2] = new PrinterRed();
+ 
+            foreach (Printer r in arr)
+            {
+                r.Print("Hello");
+            }
+ 
+            ((PrinterRed)arr[2]).Print("ad");
+        }
+ 
+    }
